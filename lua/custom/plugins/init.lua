edfe0cbc984dea1c0e8297aa51c9ef6187ddf84c
Mario Wibisono <kobamkode@gmail.com>
2025-02-26 06:35:23 +0700

    update nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,4 +34,38 @@ return {
     event = 'CmdlineEnter',
     opts = {},
   },
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*',
+    lazy = false,
+    ft = 'markdown',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'devlog',
+          path = '/home/mario/Codes/devlog/',
+          strict = true,
+        },
+      },
+      notes_subdir = '+',
+      new_notes_location = 'notes_subdir',
+      templates = {
+        folder = 'x/templates',
+      },
+      follow_url_func = function(url)
+        vim.fn.jobstart { 'xdg-open', url } -- linux
+      end,
+      mappings = {
+        ['<cr>'] = {
+          action = function()
+            return require('obsidian').util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
+    },
+  },
 }
