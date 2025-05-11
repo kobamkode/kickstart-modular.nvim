349c57217a1f722df336c2b0b7d55c06b360a2e7
Mario Wibisono <kobamkode@gmail.com>
2025-05-11 15:16:13 +0700

    update config

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,7 +1,51 @@
 return {
   {
     'olimorris/codecompanion.nvim',
-    opts = {},
+    opts = {
+      strategies = {
+        chat = {
+          adapter = 'anthropic',
+        },
+        inline = {
+          adapter = 'anthropic',
+        },
+        cmd = {
+          adapter = 'anthropic',
+        },
+      },
+      adapters = {
+        anthropic = function()
+          return require('codecompanion.adapters').extend('anthropic', {
+            env = {
+              api_key = '',
+            },
+          })
+        end,
+      },
+      prompt_library = {},
+      extensions = {
+        mcphub = {
+          callback = 'mcphub.extensions.codecompanion',
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+        history = {
+          enabled = true,
+          opts = {
+            keymap = 'gh',
+            auto_generate_title = true,
+            continue_last_chat = false,
+            delete_on_clearing_chat = false,
+            picker = 'snacks',
+            enable_logging = false,
+            dir_to_save = vim.fn.stdpath 'data' .. '/codecompanion-history',
+          },
+        },
+      },
+    },
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-treesitter/nvim-treesitter',
@@ -13,60 +57,6 @@ return {
         cmd = 'MCPHub',
         build = 'npm install -g mcp-hub@latest',
         config = true,
-      },
-      -- {
-      --   'Davidyz/VectorCode',
-      --   version = '*',
-      --   build = 'pipx upgrade vectorcode',
-      --   dependencies = { 'nvim-lua/plenary.nvim' },
-      -- },
-    },
-    adapters = {
-      anthropic = function()
-        return require('codecompanion.adapters').extend('anthropic', {
-          env = {
-            api_key = '',
-          },
-        })
-      end,
-    },
-    prompt_library = {},
-    strategies = {
-      chat = {
-        adapter = 'anthropic',
-      },
-      inline = {
-        adapter = 'anthropic',
-      },
-      cmd = {
-        adapter = 'anthropic',
-      },
-    },
-    extensions = {
-      mcphub = {
-        callback = 'mcphub.extensions.codecompanion',
-        opts = {
-          make_vars = true,
-          make_slash_commands = true,
-          show_result_in_chat = true,
-        },
-      },
-      history = {
-        enabled = true,
-        opts = {
-          keymap = 'gh',
-          auto_generate_title = true,
-          continue_last_chat = false,
-          delete_on_clearing_chat = false,
-          picker = 'snacks',
-          enable_logging = false,
-          dir_to_save = vim.fn.stdpath 'data' .. '/codecompanion-history',
-        },
-      },
-      vectorcode = {
-        opts = {
-          add_tool = true,
-        },
       },
     },
   },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,11 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    keys = {
+      { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
+    },
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    lazy = false,
+  },
+}
