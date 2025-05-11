03418a8305ee0b61c9e4aa741de833ec0c1807f2
Mario Wibisono <kobamkode@gmail.com>
2025-05-11 11:24:25 +0700

    add codecompanion

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    'olimorris/codecompanion.nvim',
+    opts = {},
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'j-hui/fidget.nvim',
+      'ravitemer/mcphub.nvim',
+      'ravitemer/codecompanion-history.nvim',
+      {
+        'ravitemer/mcphub.nvim',
+        cmd = 'MCPHub',
+        build = 'npm install -g mcp-hub@latest',
+        config = true,
+      },
+      -- {
+      --   'Davidyz/VectorCode',
+      --   version = '*',
+      --   build = 'pipx upgrade vectorcode',
+      --   dependencies = { 'nvim-lua/plenary.nvim' },
+      -- },
+    },
+    adapters = {
+      anthropic = function()
+        return require('codecompanion.adapters').extend('anthropic', {
+          env = {
+            api_key = '',
+          },
+        })
+      end,
+    },
+    prompt_library = {},
+    strategies = {
+      chat = {
+        adapter = 'anthropic',
+      },
+      inline = {
+        adapter = 'anthropic',
+      },
+      cmd = {
+        adapter = 'anthropic',
+      },
+    },
+    extensions = {
+      mcphub = {
+        callback = 'mcphub.extensions.codecompanion',
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true,
+        },
+      },
+      history = {
+        enabled = true,
+        opts = {
+          keymap = 'gh',
+          auto_generate_title = true,
+          continue_last_chat = false,
+          delete_on_clearing_chat = false,
+          picker = 'snacks',
+          enable_logging = false,
+          dir_to_save = vim.fn.stdpath 'data' .. '/codecompanion-history',
+        },
+      },
+      vectorcode = {
+        opts = {
+          add_tool = true,
+        },
+      },
+    },
+  },
+}
